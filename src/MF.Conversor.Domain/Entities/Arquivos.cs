438033a2c7d7f57f017e0e0c343a4f1e0b51408d
Murillo Filho <Murillo Filho@ATENDMENTO33>
2016-07-13 10:27:59 -0300

    Conversor v0.2 adição da camada de dominio

---
+++ src/MF.Conversor.Domain/Entities/Arquivos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MF.Conversor.Domain.Entities
+{
+    public class Arquivos
+    {
+        public string Nome { get; set; }
+        
+    }
+}
